a9223c8528554d5c3e6125174d8f6cc20494cba1
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2026-06-15 01:13:18 -0400

    add white balance and exponent commands

--- inverter/inverter.py
+++ inverter/inverter.py
@@ -4,32 +4,65 @@ import numpy as np
 import rawpy
 import tifffile
 import colour
+import scipy
 
 
 def main():
     # parse user-supplied arguments
     parser = argparse.ArgumentParser(prog="Neg Inverter",
                                      description="Automatically invert your film negatives")
+    # input / output
     parser.add_argument('image_path',
                         type=str, default=None,
                         help="Input RAW file to process.")
     parser.add_argument('output_path', type=str, default=None,
                         help="Output path for final processed image.")
+    # lut applications
     parser.add_argument('--input-lut', '-l',
                         type=str, default=None,
-                        help="LUT applied into working colour space.")
+                        help="1D LUT applied into working colour space.")
     parser.add_argument('--output-lut', '-L',
                         type=str, default=None,
-                        help="LUT applied out of working colour space.")
-    parser.add_argument('--icc', '-i',
+                        help="1D LUT applied out of working colour space.")
+    # icc profile embedding
+    parser.add_argument('--icc', '-p',
                         type=str, default=None,
                         help="ICC profile to embed in final TIFF export.")
-
+    # image modifiers
+    parser.add_argument('--invert', '-i',
+                        action='store_true',
+                        help="Directly invert the supplied image "
+                             "(skip 1d LUT).")
+    parser.add_argument('--map-density', '-m',
+                        action='store_true',
+                        help="Apply density to luminance mapping on output.")
+    parser.add_argument('--red-balance', '-r',
+                        type=float, default=1.0,
+                        help="Multiplier for red channel (pre-inversion).")
+    parser.add_argument('--green-balance', '-g',
+                        type=float, default=1.0,
+                        help="Multiplier for blue channel (pre-inversion).")
+    parser.add_argument('--blue-balance', '-b',
+                        type=float, default=1.0,
+                        help="Multiplier for green channel (pre-inversion).")
+    parser.add_argument('--red-power', '-R',
+                        type=float, default=1.0,
+                        help="Power for red channel (post-inversion).")
+    parser.add_argument('--green-power', '-G',
+                        type=float, default=1.0,
+                        help="Power for green channel (post-inversion).")
+    parser.add_argument('--blue-power', '-B',
+                        type=float, default=1.0,
+                        help="Power for blue channel (post-inversion).")
     args = parser.parse_args()
-    # read in 
+
+
+    # read in raw file
     with rawpy.imread(args.image_path) as raw:
-        # read in camera sensor data at 16bpp int
-        image_data_uint16 = raw.postprocess(use_camera_wb=True,
+        # read in camera sensor data at 16bpp int with D65 white balance
+        d65_balance = raw.daylight_whitebalance
+        image_data_uint16 = raw.postprocess(use_camera_wb=False,
+                                            user_wb=d65_balance,
                                             no_auto_bright=True,
                                             half_size=False,
                                             output_bps=16,
@@ -47,7 +80,45 @@ def main():
             input_lut = colour.read_LUT(args.input_lut)
             working_image = input_lut.apply(image_data_rec2020)
 
-        # apply modifications in working space
+        # perform adjustments in working colour space
+        # apply white balance
+        balanced_red_channel = working_image[:, :, 0].copy() * args.red_balance
+        balanced_green_channel = working_image[:, :, 1].copy() * args.green_balance
+        balanced_blue_channel = working_image[:, :, 2].copy() * args.blue_balance
+        working_image = np.stack([balanced_red_channel,
+                                  balanced_green_channel,
+                                  balanced_blue_channel],
+                                 axis=2)
+        # invert image directly
+        if args.invert:
+            # we calculate density from "transmittance" using log10(1/x)
+            # TODO: normalize before applying 1D output lut
+            print("WARN: Inverting the image is based on density, and may\n"
+                  "      create values above 1.0 which break output LUTs.",
+                  file=sys.stderr)
+            inverse_x_points = np.array([0.001, 0.25, 0.5, 0.75, 1.0])
+            inverse_y_points = np.log10(1.0 / inverse_x_points)
+            inverse_spline = scipy.interpolate.CubicSpline(inverse_x_points,
+                                                           inverse_y_points)
+            working_image = inverse_spline(working_image)
+        # adjust channel power
+        power_red_channel = np.power(working_image[:, :, 0].copy(),
+                                     args.red_power)
+        power_green_channel = np.power(working_image[:, :, 1].copy(),
+                                       args.green_power)
+        power_blue_channel = np.power(working_image[:, :, 2].copy(),
+                                      args.blue_power)
+        working_image = np.stack([power_red_channel,
+                                  power_green_channel,
+                                  power_blue_channel],
+                                 axis=2)
+        # map density to luminance
+        if args.map_density:
+            lum_x_points = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
+            lum_y_points = np.power(10, lum_x_points) * 0.01
+            lum_spline = scipy.interpolate.CubicSpline(lum_x_points,
+                                                       lum_y_points)
+            working_image = lum_spline(working_image)
 
         # apply output lut (if present)
         final_image = working_image
