822f272f2bde8b7f35d43a465a779405de87937b
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2026-08-05 22:15:12 -0400

    process bw negatives without preset for better contrast in batch script
    possible indication that presets need to consider more factors from the individual negative?

--- batch_conversion_bw_ffc.sh
+++ batch_conversion_bw_ffc.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # example of how to perform batch conversions using the CLI utility
-# $1 is directory, $2 is raw file extension (NEF, for example)
-# $3 is preset profile, $4 is output directory
-# $5 is ffc image
-# $6 is location of neg_inverter project
+# $1 is directory
+# $2 is raw file extension (NEF, for example)
+# $3 is output directory
+# $4 is ffc image
+# $5 is location of neg_inverter project
 
 WORK_DIR="."
 if [ -n "$6" ]; then
@@ -12,17 +13,18 @@ if [ -n "$6" ]; then
 fi
 
 for item in "$1"/*."$2"; do
-    if [ "$5" != "basename $item" ]; then
+    if [ "$4" != "basename $item" ]; then
         python3 "$WORK_DIR/inverter/inverter.py" \
             -p "$WORK_DIR/bw-profile.icc" \
-            --preset "$3" \
             --bw \
-            --crop-inset 0.95 \
-            --ffc "$5" \
+            --red-ratio 1.0 \
+            --blue-ratio 1.0 \
+            --crop-inset 0.91 \
+            --ffc "$4" \
             "$item" \
-            "$4/$(basename $item .$2).tiff"
+            "$3/$(basename "$item" ".$2").tiff"
     else
-        # this shit don't work
+        # this clause simply don't work
         echo "Skipping FFC source file."
     fi
 done
